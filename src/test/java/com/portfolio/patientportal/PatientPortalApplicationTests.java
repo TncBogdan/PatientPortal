ecd69173ddaa374a4b64c5757bb127a3d8211272
Cypris <cypris_84@yahoo.com>
2020-05-12 13:49:30 +0300

    initial commit

---
+++ src/test/java/com/portfolio/patientportal/PatientPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.patientportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientPortalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
